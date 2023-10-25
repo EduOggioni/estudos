7364bd2dbad0923a4eab9f0e074422a9e82b7d17
Eduardo Oggioni <eduardo.oggioni@hotmail.com>
2023-10-25 19:28:32 -0300

    JS exemplo03

---
+++ JS/JSWEB/exemplo03.js
@@ -0,0 +1,20 @@
+/*
+    Ótimo, vimos as formas de declarar uma variável, agora vamos para os tipos de valores que uma variável pode receber
+
+    Os valores são simples e complexos, e assim como organismos, os simples são unicelulares (único valor) e os complexos
+    são multicelulares (múltiplos valores em uma única variável)
+
+    Os simples são TEXTO, NÚMEROS e BOLEANOS em suma!
+    Textos são String
+    Números são Integer (Int) ou Float
+    Boleanos são Boolean
+*/
+
+const texto = "EDU"; // Textos, strings são sempre delimetados por aspas ou aspas duplas
+const numero = 1; // É número, não tem erro, se for FLOAT é número decimal, bem simples
+const numeroDecimal = 1.1; // Viu, mega simples
+const boleano = TRUE; // Boleanos são TRUE ou FALSE, verdadeiro ou falso
+console.log(texto);
+console.log(numero);
+console.log(numeroDecimal);
+console.log(boleano);
