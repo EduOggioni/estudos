f8f4f0d14bb65dd914c6ca2ba072e7494849db31
Eduardo Oggioni <eduardo.oggioni@hotmail.com>
2023-10-25 19:11:06 -0300

    JS iniciando e exercicio01

---
+++ JS/JSWEB/exemplo01.js
@@ -0,0 +1,3 @@
+alert("Esse alerta veio do arquivo JS");
+// Vantagem é o reuso e ajuda abstraindo complexidade facilitando a leitura.
+// Outro ponto de vantagem é que um arquivo JS pode chamar outro e outro e outro, fazendo um cadeia de arquivos, mas isto veremos no FUTURO.
