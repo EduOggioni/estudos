3c6b7a7cf16f29d8d85c1269b4a0e46a94f03b95
Diana <diana.vaillant15@gmail.com>
2023-10-25 20:13:55 -0300

    Exercicio 4 solucao

---
+++ JS/JSWEB/exercicio03.js
@@ -0,0 +1,14 @@
+let idade = 25;
+let altura = 1.59;
+const nome = "Diana";
+const mulher = true;
+const array = [nome, idade, altura];
+console.log(array);
+
+const diana = {
+    prop1: mulher,
+    prop2: nome
+};
+
+console.log(diana);
+
